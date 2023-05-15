b05ba7d741c0430963cec6a50a479b2eb83d2b67
mstrucka <mstrucka@gmail.com>
2023-05-15 21:15:40 +0200

    save refactor, update and list All missing, token and credential hardcoded

--- springBootProject/script.js
+++ springBootProject/script.js
@@ -1,60 +1,61 @@
-//Get
+var token = ''; // Replace with your JWT token
+
 function getAllSchoolsFromTable(){
-   axios.get('http://localhost:8090/school/all')
-     .then((res) => console.log(res))
-     .catch((err0 => console.log(err)))
- }
- 
- //Post
- class School {
-   constructor(schoolId, address, capacity, name) {
-     this.schoolId = schoolId;
-     this.address = address;
-     this.capacity = capacity;
-     this.name = name;
-   }
- }
- 
- function handleSubmit(event) {
-   event.preventDefault();
-   const formData = new FormData(event.target);
-   const school = new School(
-     formData.get('schoolId'),
-     formData.get('address'),
-     formData.get('capacity'),
-     formData.get('name')
-   );
-   axios.post('http://localhost:8090/school/save', school)
-     .then(response => {
-       console.log(response.data);
-     })
-     .catch(error => {
-       console.log(error);
-     });
- }
- 
- function App() {
-   return (
-     <form onSubmit={handleSubmit} id="myform">
-       <div>
-         <label htmlFor="schoolId">schoolId</label>
-         <input type="number" name="schoolId" id="schoolId" />
-       </div>
-       <div>
-         <label htmlFor="name">Name</label>
-         <input type="text" name="name" id="name" />
-       </div>
-       <div>
-         <label htmlFor="address">Address</label>
-         <input type="text" name="address" id="address" />
-       </div>
-       <div>
-         <label htmlFor="capacity">Capacity</label>
-         <input type="number" name="capacity" id="capacity" />
-       </div>
-       <button type="submit">Submit</button>
-     </form>
-   );
- }
- 
- ReactDOM.render(<App />, document.getElementById('root'));
+  axios.get('http://localhost:8090/school/all')
+    .then((res) => console.log(res))
+    .catch((err0 => console.log(err)))
+}
+
+// hardcoded login to retrieve the token for later use
+document.getElementById('getTokenButton').addEventListener('click', function() {
+  var email = 'haha@hihi.com';
+  var password = 'PassW0rd1';
+
+  var loginDto = {
+    email: email,
+    password: password
+  };
+
+  axios.post('http://localhost:8090/user/login', loginDto)
+    .then(function(response) {
+      token = response.data;
+      console.log('Token:', response.data);
+      // Do something with the token
+    })
+    .catch(function(error) {
+      console.error('Error retrieving token:', error);
+      // Handle the error
+    });
+});
+
+// creating school
+document.getElementById('createSchoolForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  var id = document.getElementById('id').value;
+  var name = document.getElementById('name').value;
+  var capacity = document.getElementById('capacity').value;
+  var address = document.getElementById('address').value;
+
+  var school = {
+    id: id,
+    name: name,
+    capacity: capacity,
+    address: address
+  };
+
+
+  axios.post('http://localhost:8090/school/save', school, { // address
+    headers: {
+      'Authorization': 'Bearer ' + token
+    }
+  })
+  .then(function(response) {
+    console.log('School created successfully:', response.data);
+    // Do something with the response
+  })
+  .catch(function(error) {
+    console.error('Error creating school:', error);
+    // Handle the error
+  });
+});
